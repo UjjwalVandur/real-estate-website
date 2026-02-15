5181989bd2887d47bbebc9891dc0b6058aa4b2cc
UjjwalVandur <ujjwalvandur@gmail.com>
2026-02-15 17:20:15 +0530

    Fix session cookie for cross-site authentication

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production',
+    // secure: process.env.NODE_ENV === 'production',
+    secure: true,
     httpOnly: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
